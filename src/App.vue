f27788ac991c5108f4d0d5c94baea7eda1140ca2
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-10-05 16:06:38 +0800

    component setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import PwdListComponent from "./components/PwdListComponent.vue";
+</script>
+
+<template>
+  <header-component />
+  <pwd-list-component />
+</template>
+
+<style scoped></style>
---
+++ src/components/PwdListComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import PwdItemComponent from "./PwdItemComponent.vue";
+</script>
+
+<template>
+  <ul>
+    <li><pwd-item-component /></li>
+    <li><pwd-item-component /></li>
+    <li><pwd-item-component /></li>
+    <li><pwd-item-component /></li>
+  </ul>
+</template>
+
+<style scoped></style>
