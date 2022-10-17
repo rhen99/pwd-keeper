0ae06169a5e5a6dded65e897b2600b1bc5fdd302
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-10-17 13:57:07 +0800

    setup routes and views

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <script setup>
-import { reactive } from "vue";
+import { RouterView } from "vue-router";
 import HeaderComponent from "./components/HeaderComponent.vue";
-import PwdListComponent from "./components/PwdListComponent.vue";
-
-const passwordList = reactive([
-  {
-    title: "Password 1",
-    password: "12345",
-  },
-  {
-    title: "Password 2",
-    password: "12345",
-  },
-  {
-    title: "Password 3",
-    password: "12345",
-  },
-  {
-    title: "Password 4",
-    password: "12345",
-  },
-]);
 </script>
 
 <template>
   <header-component />
-  <pwd-list-component :passwordList="passwordList" />
+  <router-view />
 </template>
 
 <style scoped></style>
